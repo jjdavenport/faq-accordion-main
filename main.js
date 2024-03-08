e9927d9004187b122781859ae79c174c7eaf9dc5
jjdavenport <jordanjamesdavenport@gmail.com>
2024-03-08 18:50:33 +0000

    function to display answer

---
+++ main.js
@@ -0,0 +1,29 @@
+const b1 = document.getElementById('b1');
+const a1 = document.getElementById('1');
+
+b1.addEventListener('click', function(){
+    a1.classList.toggle('a-active')
+})
+
+const b2 = document.getElementById('b2');
+const a2 = document.getElementById('2');
+
+b2.addEventListener('click', function(){
+    a2.classList.toggle('a-active')
+})
+
+
+const b3 = document.getElementById('b3');
+const a3 = document.getElementById('3');
+
+b3.addEventListener('click', function(){
+    a3.classList.toggle('a-active')
+})
+
+
+const b4 = document.getElementById('b4');
+const a4 = document.getElementById('4');
+
+b4.addEventListener('click', function(){
+    a4.classList.toggle('a-active')
+})
